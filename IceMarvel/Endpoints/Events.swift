b94e0b8d27fd13009fdabf9348388520be006cbe
jabernall24 <jabernall24@gmail.com>
2019-03-21 18:56:37 -0700

    setup remaining endpoint files

---
+++ IceMarvel/Endpoints/Events.swift
@@ -0,0 +1,13 @@
+//
+//  Events.swift
+//  IceMarvel
+//
+//  Created by Jesus Andres Bernal Lopez on 3/21/19.
+//  Copyright © 2019 Jesus Andres Bernal Lopez. All rights reserved.
+//
+
+import Foundation
+
+extension IceMarvel {
+    
+}
